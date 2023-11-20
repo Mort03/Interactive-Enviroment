d2e1f13900b3e01251cf91dc92c6b52f965e1ccb
Mort03 <emil.linnarsson@gmail.com>
2023-11-20 14:41:58 +0100

    Added Blender Model
    A table added.

--- Assets/Scripts/CameraMoveScript.cs
+++ Assets/Scripts/CameraMoveScript.cs
@@ -8,23 +8,34 @@ public class CameraMoveScript : MonoBehaviour
     public Transform target;
     public float followDistance = 0f;
     public float cameraSpeed = 3f;
-    public Transform targetEyes;
+    public Transform table;
 
     //Körs efter varje frame
     private void LateUpdate()
     {
-
         float newRotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * cameraSpeed;
         float newRotationY = transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * cameraSpeed;
 
         Vector3 desiredRotation = new Vector3( newRotationY, newRotationX, 0f);
+        Vector3 desiredTargetRotation = new Vector3(0f, newRotationX, 0f);
+
         transform.localEulerAngles = desiredRotation;
+        target.localEulerAngles = desiredTargetRotation;
 
         //The players "forward" is the cameras "forward"
-        //In other words you look forward and the character model follows
-        target.forward = transform.forward;
+        //In other words, you look left, the character model moves left (in x-axis)
+        //target.forward = transform.forward;
 
-        Vector3 desiredPosition = target.position - transform.forward * followDistance;
-        transform.position = desiredPosition;
+        float positionY = target.position.y + 1;
+        float positionX = target.position.x;
+        float positionZ = target.position.z;
+
+        //if i ever need the other line of code for target position:
+        //Vector3 desiredPosition = target.position - transform.forward * followDistance;
+
+        Vector3 desiredHeadLevel = new Vector3 ( positionX, positionY, positionZ);
+
+        
+        transform.position = desiredHeadLevel;
     }
 }
